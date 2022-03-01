0af56d956c808e0b697c55479c875d69ef6a984d
lakk1 <luckylaxman121@gmail.com>
2022-03-01 15:23:14 +0530

    Layout

---
+++ client/components/Layout/index.tsx
@@ -0,0 +1,22 @@
+import { Box, useColorModeValue } from "@chakra-ui/react";
+import Navbar from "./Navbar";
+
+const RootLayout = (props: { children: any }) => {
+  const { children } = props;
+
+  return (
+    <Box
+      px={{
+        base: 20,
+        md: 36,
+      }}
+      height="100vh"
+      bg={useColorModeValue("#f2f4f6", "#1c1c21")}
+    >
+      <Navbar />
+      <Box>{children}</Box>{" "}
+    </Box>
+  );
+};
+
+export default RootLayout;
