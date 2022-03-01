a41e8ae4ca7bcaf926d9384463dcd7d1d5d8a67a
lakk1 <luckylaxman121@gmail.com>
2022-03-01 15:18:52 +0530

    add new colorscheme

--- client/theme.ts
+++ client/theme.ts
@@ -1,10 +1,26 @@
-import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
+import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
 
 const config: ThemeConfig = {
-  initialColorMode: 'dark',
+  initialColorMode: "light",
   useSystemColorMode: false,
-}
+};
 
-const theme = extendTheme({ config })
+const theme = extendTheme({
+  config,
+  colors: {
+    newblue: {
+      "50": "#E5F6FF",
+      "100": "#B8E5FF",
+      "200": "#8AD5FF",
+      "300": "#5CC4FF",
+      "400": "#2EB4FF",
+      "500": "#00A3FF",
+      "600": "#0082CC",
+      "700": "#006299",
+      "800": "#004166",
+      "900": "#002133",
+    },
+  },
+});
 
-export default theme
+export default theme;
