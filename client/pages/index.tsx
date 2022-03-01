387dab9d4c9badda6944d43d7eceb1a19a397a99
lakk1 <luckylaxman121@gmail.com>
2022-03-01 15:24:52 +0530

    Update main page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,16 +1,10 @@
+import { Box } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import Head from "next/head";
+import RootLayout from "components/Layout";
+import SwapForm from "components/SwapForm";
 
 const Home: NextPage = () => {
-  const connect = async () => {
-    if (typeof window.ethereum !== "undefined") {
-      const accounts = await window.ethereum.request({
-        method: "eth_requestAccounts",
-      });
-
-      console.log(accounts);
-    }
-  };
   return (
     <div>
       <Head>
@@ -19,15 +13,20 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1>
-          Welcome to lakiswap
-        </h1>
-
-        <button onClick={connect}>connect</button>
-      </main>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          mt: 8,
+        }}
+      >
+        <SwapForm />
+      </Box>
     </div>
   );
 };
 
+Home.getLayout = (page) => <RootLayout>{page}</RootLayout>;
 export default Home;
