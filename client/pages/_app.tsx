6008a012a8fde3f1694f52c66d3aecb23e81b33d
lakk1 <luckylaxman121@gmail.com>
2022-03-01 15:24:18 +0530

    _app with web3provider and themeProvider

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,12 +1,30 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Web3ReactProvider } from "@web3-react/core";
 import theme from "../theme";
+import { ethers } from "ethers";
+import { ReactElement, ReactNode, ComponentType } from "react";
+import { NextPage } from "next";
 
-function MyApp({ Component, pageProps }: AppProps) {
+const getLibrary = (provider: any) => {
+  return new ethers.providers.Web3Provider(provider);
+};
+
+export type Page<P = {}> = NextPage<P> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+type Props = AppProps & {
+  Component: Page;
+};
+
+function MyApp({ Component, pageProps }: Props) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <Web3ReactProvider getLibrary={getLibrary}>
+        {getLayout(<Component {...pageProps} />)}
+      </Web3ReactProvider>
     </ChakraProvider>
   );
 }
